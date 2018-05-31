c1a57445515043bf05a92096721d2a8251119e2e
Frizlab <fload@me.com>
2018-05-31 13:51:24 +0200

    Fix some threading fails in the Connector

--- Sources/ghapp/Connectors/Connector.swift
+++ Sources/ghapp/Connectors/Connector.swift
@@ -42,6 +42,8 @@ class Connector<Helper : ConnectorHelper> {
 	typealias Scope = Helper.Scope
 	typealias RequestType = Helper.RequestType
 	
+	let helper: Helper
+	
 	init(helper h: Helper) {
 		helper = h
 	}
@@ -129,14 +131,11 @@ class Connector<Helper : ConnectorHelper> {
 		startNextAction()
 	}
 	
-	/** Checks the active session of the connected social network for validity.
-	
-	A side effect of this method could be the disconnection of to social network
-	(if for instance the tokens are no longer valid for an OAuth connector), but
-	the method will try refreshing the session first if refreshIfExpired is true.
+	/** If there is already an action in progress, the action will be delayed
+	until the current (and all other currently queued) action(s) are finished.
 	
 	The handler is always called on the main thread. */
-	func checkSession(forScope scope: Scope, autoRefresh refreshIfExpired: Bool, handler: ((_ error: Error?) -> Void)?) {
+	func checkSession(forScope scope: Scope, handler: ((_ error: Error?) -> Void)?) {
 		assert(Thread.isMainThread)
 		
 		queuedActions.append{
@@ -145,45 +144,34 @@ class Connector<Helper : ConnectorHelper> {
 			})
 		}
 		startNextAction()
-//		active = true
-//		_checkSession(forScope: scope) { checkSessionError in
-//			let checkSessionError = checkSessionError as NSError?
-//			let checkSessionSuccess = (checkSessionError == nil)
-//			if refreshIfExpired && !checkSessionSuccess && checkSessionError?.code == HCError.tokenExpired.rawValue && checkSessionError?.domain == HC_ERR_DOMAIN {
-//				DispatchQueue.main.async {
-//					self._refreshSession { refreshSessionError in
-//						if refreshSessionError == nil {
-//							self._checkSession(forScope: scope) { err in
-//								self.finishAction(handler: handler, error: err)
-//							}
-//						} else {
-//							self.finishAction(handler: handler, error: checkSessionError)
-//						}
-//					}
-//				}
-//			} else {
-//				self.finishAction(handler: handler, error: checkSessionError)
-//			}
-//		}
 	}
 	
-	private let helper: Helper
+	/* ***************
+      MARK: - Private
+	   *************** */
 	
 	private var queuedActions = [() -> Void]()
+	private var actionQueue = DispatchQueue(label: "A Connector Queue", qos: .utility)
 	private var actionSemaphore = DispatchSemaphore(value: 1)
 	
 	private func startNextAction() {
-		guard actionSemaphore.wait(timeout: .now()) == .success else {return}
-		guard let action = queuedActions.popLast() else {actionSemaphore.signal(); return}
-		action()
+		actionQueue.async{
+			guard self.actionSemaphore.wait(timeout: .now()) == .success else {return}
+			guard let action = self.queuedActions.popLast() else {self.actionSemaphore.signal(); return}
+			action()
+		}
 	}
 	
 	private func finishAction(error: Error?, handler: ((Error?) -> Void)?) {
-		actionSemaphore.signal()
-		startNextAction()
-		
 		if let h = handler {
-			DispatchQueue.main.async{ h(error) }
+			DispatchQueue.main.async{
+				h(error)
+				self.actionSemaphore.signal()
+				self.startNextAction()
+			}
+		} else {
+			actionSemaphore.signal()
+			startNextAction()
 		}
 	}
 	
