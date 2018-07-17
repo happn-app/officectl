9bfaae682a97f5a9458fbdea16a0b04ffc2c816f
Frizlab <fload@me.com>
2018-07-17 20:54:33 +0200

    Convert the get-token operation to Futures

--- Sources/officectl/Commands/root/get-token.swift
+++ Sources/officectl/Commands/root/get-token.swift
@@ -8,40 +8,25 @@
 import Foundation
 
 import Guaka
+import NIO
 
 import OfficeKit
 
 
 
-class GetTokenOperation : CommandOperation {
-	/*
-	let googleConnectorOperation: GetConnectedGoogleConnector
-	
-	override init(command c: Command, flags f: Flags, arguments args: [String]) {
-		do {
-			let scopes = f.getString(name: "scopes")!
-			let userBehalf = f.getString(name: "google-admin-email")!
-			googleConnectorOperation = try GetConnectedGoogleConnector(flags: f, scope: Set(scopes.components(separatedBy: ",")), userBehalf: userBehalf)
-		} catch {
-			c.fail(statusCode: (error as NSError).code, errorMessage: error.localizedDescription)
+func getToken(flags f: Flags, arguments args: [String], asyncConfig: AsyncConfig) -> EventLoopFuture<Void> {
+	do {
+		let scopes = f.getString(name: "scopes")!
+		let userBehalf = f.getString(name: "google-admin-email")!
+		let googleConnector = try GoogleJWTConnector(flags: f, userBehalf: userBehalf)
+		
+		let f = googleConnector.connect(scope: Set(scopes.components(separatedBy: ",")), asyncConfig: asyncConfig)
+		.then{ _ -> EventLoopFuture<Void> in
+			print(googleConnector.token!)
+			return asyncConfig.eventLoop.newSucceededFuture(result: ())
 		}
-		
-		super.init(command: c, flags: f, arguments: args)
-		
-		addDependency(googleConnectorOperation)
+		return f
+	} catch {
+		return asyncConfig.eventLoop.newFailedFuture(error: error)
 	}
-	
-	override func startBaseOperation(isRetry: Bool) {
-		if let e = googleConnectorOperation.connectionError as NSError? {
-			command.fail(statusCode: e.code, errorMessage: e.localizedDescription)
-		}
-		
-		print(googleConnectorOperation.connector.token!)
-		baseOperationEnded()
-	}
-	
-	override var isAsynchronous: Bool {
-		return false
-	}*/
-	
 }
