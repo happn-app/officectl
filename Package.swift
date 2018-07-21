0c8fc0aaf62b2b42212b69fb248ca36422b5c8f5
Frizlab <fload@me.com>
2018-07-22 01:49:43 +0200

    Update dependencies to Linux compatible versions

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
 	name: "officectl",
 	dependencies: [
 		.package(url: "git@github.com:happn-app/AsyncOperationResult.git", from: "1.0.4"),
-		.package(url: "git@github.com:happn-app/URLRequestOperation.git", from: "1.0.0"),
-		.package(url: "git@github.com:happn-app/RetryingOperation.git", from: "1.1.0"),
+		.package(url: "git@github.com:happn-app/URLRequestOperation.git", from: "1.1.2"),
+		.package(url: "git@github.com:happn-app/RetryingOperation.git", from: "1.1.1"),
 		.package(url: "git@github.com:happn-app/Swift-OpenLDAP.git", from: "1.0.0"),
 		.package(url: "git@github.com:happn-app/Swift-CommonCrypto.git", from: "1.0.0"),
 		.package(url: "https://github.com/apple/swift-nio.git", from: "1.8.0"),
