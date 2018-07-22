12407bc1e25e1adfbb750a069ed3c19983c2ae6d
Frizlab <fload@me.com>
2018-07-22 11:47:32 +0200

    Add Crypto dependency from Vapor

--- Package.swift
+++ Package.swift
@@ -12,10 +12,11 @@ let package = Package(
 		.package(url: "git@github.com:happn-app/Swift-CommonCrypto.git", from: "1.0.0"),
 		.package(url: "https://github.com/apple/swift-nio.git", from: "1.8.0"),
 		.package(url: "https://github.com/klaas/Guaka.git", from: "0.3.0"),
+		.package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-rc"),
 		.package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc")
 	],
 	targets: [
-		.target(name: "OfficeKit", dependencies: ["AsyncOperationResult", "RetryingOperation", "URLRequestOperation", "NIO", "FluentSQLite"]),
+		.target(name: "OfficeKit", dependencies: ["AsyncOperationResult", "RetryingOperation", "URLRequestOperation", "NIO", "FluentSQLite", "Crypto"]),
 		.target(name: "officectl", dependencies: ["OfficeKit", "Guaka"]),
 		.testTarget(name: "OfficeKitTests", dependencies: ["OfficeKit"])
 	]	
