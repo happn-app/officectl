2764bc72033ce03c1300a12a0f65ad71af9d8371
Frizlab <fload@me.com>
2018-08-09 15:20:59 +0200

    Implement show reset page method

--- Sources/officectl/Server/PasswordResetController.swift
+++ Sources/officectl/Server/PasswordResetController.swift
@@ -9,12 +9,30 @@ import Foundation
 
 import Vapor
 
+import OfficeKit
+
 
 
 final class PasswordResetController {
 	
-	func index(_ req: Request) throws -> Future<View> {
+	func showUserSelection(_ req: Request) throws -> Future<View> {
 		return try req.view().render("PasswordResetUserSelection")
+	}
+	
+	func showResetPage(_ req: Request) throws -> String {
+		let emailStr = try req.parameters.next(String.self)
+		
+		/* Let’s validate the email */
+		guard let email = Email(string: emailStr) else {throw NSError(domain: "com.happn.officectl", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid email"])}
+		/* Only happn.fr domain supported for now */
+		guard email.domain == "happn.fr" else {throw NSError(domain: "com.happn.officectl", code: 1, userInfo: [NSLocalizedDescriptionKey: "Only happn.fr emails are supported for now"])}
+		/* Only “regular” username (no fancy characters are allowed) */
+		let regex = try! NSRegularExpression(pattern: "[^0-9a-z_.-]", options: [])
+		guard regex.firstMatch(in: email.username, options: [], range: NSRange(email.username.startIndex..<email.username.endIndex, in: email.username)) == nil else {
+			throw NSError(domain: "com.happn.officectl", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid character in username"])
+		}
+		
+		return emailStr
 	}
 	
 }
--- Sources/officectl/Server/setup_routes.swift
+++ Sources/officectl/Server/setup_routes.swift
@@ -18,5 +18,6 @@ func setup_routes(_ router: Router) throws {
 	}
 	
 	let resetPasswordController = PasswordResetController()
-	router.get("password-reset", use: resetPasswordController.index)
+	router.get("password-reset", use: resetPasswordController.showUserSelection)
+	router.get("password-reset", PathComponent.parameter("string"), use: resetPasswordController.showResetPage)
 }
