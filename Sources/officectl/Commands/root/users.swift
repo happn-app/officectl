3030ccddff09436b60b6f35a0c78fb99c3eba9c1
Frizlab <fload@me.com>
2018-08-24 11:46:51 +0200

    Create the "users delete" command

---
+++ Sources/officectl/Commands/root/users.swift
@@ -0,0 +1,19 @@
+/*
+ * users.swift
+ * officectl
+ *
+ * Created by François Lamboley on 20/08/2018.
+ */
+
+import Foundation
+
+import Guaka
+import Vapor
+
+import OfficeKit
+
+
+
+func users(flags f: Flags, arguments args: [String], context: CommandContext) throws -> EventLoopFuture<Void> {
+	throw NSError(domain: "com.happn.officectl", code: 1, userInfo: [NSLocalizedDescriptionKey: "subcommand is required"])
+}
