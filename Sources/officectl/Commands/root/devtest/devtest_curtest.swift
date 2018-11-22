49830241dd4ceb1770531cdf40a2041bbf3ad2a3
Frizlab <fload@me.com>
2018-11-22 11:23:54 +0100

    Change the current test

--- Sources/officectl/Commands/root/devtest/devtest_curtest.swift
+++ Sources/officectl/Commands/root/devtest/devtest_curtest.swift
@@ -11,12 +11,26 @@ import Guaka
 import Vapor
 
 import OfficeKit
+import URLRequestOperation
 
 
 
 func curTest(flags f: Flags, arguments args: [String], context: CommandContext) throws -> EventLoopFuture<Void> {
 	let asyncConfig: AsyncConfig = try context.container.make()
 	
+//	let c = try GitHubJWTConnector(flags: f)
+//	let f = c.connect(scope: (), asyncConfig: asyncConfig)
+//	.then{ _ -> Future<[GitHubRepository]> in
+//		let op = GitHubRepositorySearchOperation(searchedOrganisation: "happn-app", gitHubConnector: c)
+//		return asyncConfig.eventLoop.future(from: op, queue: asyncConfig.operationQueue, resultRetriever: { try $0.result.successValueOrThrow() })
+//	}
+//	.then{ repositories -> EventLoopFuture<Void> in
+//		for r in repositories {
+//			print("https://github.com/" + r.fullName + "/settings/installations")
+//		}
+//		return asyncConfig.eventLoop.newSucceededFuture(result: ())
+//	}
+//	return f
 	let c = try GoogleJWTConnector(flags: f, userBehalf: f.getString(name: "google-admin-email")!)
 	let f = c.connect(scope: ModifyGoogleUserOperation.scopes, asyncConfig: asyncConfig)
 	.then{ _ -> EventLoopFuture<GoogleUser> in
