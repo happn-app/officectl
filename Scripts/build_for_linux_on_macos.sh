447f7466b10d473d88b95c10e4b86e1dde7d8c14
Frizlab <fload@me.com>
2019-01-17 21:36:53 +0100

    Add some more logs in the build for Linux script

--- Scripts/build_for_linux_on_macos.sh
+++ Scripts/build_for_linux_on_macos.sh
@@ -13,6 +13,12 @@ officectl_builder_image_name="officectl-builder:latest"
 cd "$(dirname "$0")"
 docker build -t "$officectl_builder_image_name" .
 
+echo
+echo
+echo
+echo "*** Building officectl with treeish ${treeish:-<none, just merging current upstream if possible>}."
+echo
+
 cd ..
 BUILD_FOLDER_PATH="$(pwd)/linux_build"
 docker run --rm -v "$BUILD_FOLDER_PATH":"/mnt/output" -v "$HOME/.ssh/id_rsa":"/root/.ssh/id_rsa" -v "$(pwd)/Scripts/ask_pass.sh":"/usr/local/bin/ask_pass.sh" -e SSH_ASKPASS="/usr/local/bin/ask_pass.sh" -e DISPLAY="dummy" "$officectl_builder_image_name" "git@github.com:happn-app/officectl.git=$treeish"
