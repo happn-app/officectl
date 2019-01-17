857789966fc90ddd5a382493b532c238d31bc742
Frizlab <fload@me.com>
2019-01-17 21:55:11 +0100

    Enhance the build_for_linux_on_macos script

--- Scripts/build_for_linux_on_macos.sh
+++ Scripts/build_for_linux_on_macos.sh
@@ -2,16 +2,29 @@
 
 set -e
 
+readonly OFFICECTL_BUILDER_IMAGE_NAME="officectl-builder:latest"
+
+readonly SCRIPT_DIR="$(pwd)/$(dirname "$0")"
+readonly ASSETS_DIR="$SCRIPT_DIR/zz_assets"
+readonly ASK_PASS_PATH="$SCRIPT_DIR/ask_pass.sh"
+
+readonly treeish="$1"
+
+cd "$SCRIPT_DIR/.."
+
+
 if [ "$(uname)" != "Darwin" ]; then
 	echo "This script should be run on macOS" >/dev/stderr
 	exit 1
 fi
 
-treeish=$1
-officectl_builder_image_name="officectl-builder:latest"
+if [ ! -x "$ASK_PASS_PATH" ]; then
+	echo "$ASK_PASS_PATH: executable not found (needed to input the password of the ssh key; can be an empty script if key does not have a password)." >/dev/stderr
+	exit 1
+fi
 
-cd "$(dirname "$0")"
-docker build -t "$officectl_builder_image_name" .
+
+docker build -t "$OFFICECTL_BUILDER_IMAGE_NAME" "$ASSETS_DIR"
 
 echo
 echo
@@ -19,6 +32,5 @@ echo
 echo "*** Building officectl with treeish ${treeish:-<none, just merging current upstream if possible>}."
 echo
 
-cd ..
 BUILD_FOLDER_PATH="$(pwd)/linux_build"
-docker run --rm -v "$BUILD_FOLDER_PATH":"/mnt/output" -v "$HOME/.ssh/id_rsa":"/root/.ssh/id_rsa" -v "$(pwd)/Scripts/ask_pass.sh":"/usr/local/bin/ask_pass.sh" -e SSH_ASKPASS="/usr/local/bin/ask_pass.sh" -e DISPLAY="dummy" "$officectl_builder_image_name" "git@github.com:happn-app/officectl.git=$treeish"
+docker run --rm -v "$BUILD_FOLDER_PATH":"/mnt/output" -v "$HOME/.ssh/id_rsa":"/root/.ssh/id_rsa" -v "$ASK_PASS_PATH":"/usr/local/bin/ask_pass.sh" -e SSH_ASKPASS="/usr/local/bin/ask_pass.sh" -e DISPLAY="dummy" "$OFFICECTL_BUILDER_IMAGE_NAME" "git@github.com:happn-app/officectl.git=$treeish"
