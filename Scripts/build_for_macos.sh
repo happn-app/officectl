77676540c82e09a8c9af669e0f1059f2d7831e60
Frizlab <fload@me.com>
2019-03-18 11:06:10 +0100

    Update dependencies

--- Scripts/build_for_macos.sh
+++ Scripts/build_for_macos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/.."
-swift build -c release -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" --static-swift-stdlib
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/local/opt/openssl/lib/pkgconfig:${HOME}/usr/homebrew/opt/openssl/lib/pkgconfig" swift build -c release -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" --static-swift-stdlib
--- Scripts/generate_xcodeproj.sh
+++ Scripts/generate_xcodeproj.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/.."
-swift package generate-xcodeproj --xcconfig-overrides ./Package.xcconfig
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/local/opt/openssl/lib/pkgconfig:${HOME}/usr/homebrew/opt/openssl/lib/pkgconfig" swift package generate-xcodeproj --xcconfig-overrides ./Package.xcconfig
