59ea6cfd249477a6666a17158628d86250e44ae8
Frizlab <fload@me.com>
2019-04-02 17:12:41 +0200

    Update Package dependencies

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
 		.macOS(.v10_13)
 	],
 	dependencies: [
-		.package(url: "https://github.com/happn-tech/AsyncOperationResult.git", from: "1.0.4"),
 		.package(url: "https://github.com/happn-tech/URLRequestOperation.git", from: "1.1.2"),
 		.package(url: "https://github.com/happn-tech/RetryingOperation.git", from: "1.1.1"),
 		.package(url: "https://github.com/happn-tech/SemiSingleton.git", from: "2.0.0"),
@@ -24,7 +23,7 @@ let package = Package(
 		.package(url: "https://github.com/vapor/jwt.git", from: "3.0.0")
 	],
 	targets: [
-		.target(name: "OfficeKit", dependencies: ["AsyncOperationResult", "RetryingOperation", "URLRequestOperation", "SemiSingleton", "NIO", "FluentSQLite", "Crypto", "Vapor", "JWT"]),
+		.target(name: "OfficeKit", dependencies: ["RetryingOperation", "URLRequestOperation", "SemiSingleton", "NIO", "FluentSQLite", "Crypto", "Vapor", "JWT"]),
 		.target(name: "officectl", dependencies: ["OfficeKit", "Vapor", "Leaf", "Guaka", "Yaml", "JWT"]),
 		.testTarget(name: "OfficeKitTests", dependencies: ["OfficeKit"])
 	]	
