b15a4937f5278d8c5fa5fd372f48d9a6b6232d40
Frizlab <fload@me.com>
2019-04-16 18:11:16 +0200

    Fix SyncOperationQueue

--- Sources/OfficeKit/Utilities/Operations/SyncOperationQueue.swift
+++ Sources/OfficeKit/Utilities/Operations/SyncOperationQueue.swift
@@ -12,9 +12,15 @@ import Foundation
 /** An operation queue which does not allow concurrent operation. */
 public class SyncOperationQueue : OperationQueue {
 	
+	public override init() {
+		super.init()
+		
+		super.maxConcurrentOperationCount = 1
+	}
+	
 	public override var maxConcurrentOperationCount: Int {
 		get {
-			return 1
+			return super.maxConcurrentOperationCount
 		}
 		set {
 			fatalError("Attempted to change the maximum number of concurrent operation on a SyncOperationQueue. This is forbidden.")
