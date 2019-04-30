9248cb6683f9991b3eeffc176f4664918a1e6217
Frizlab <fload@me.com>
2019-04-30 18:20:37 +0200

    Disable sandbox deactivation as we don’t use it anymore

--- Scripts/build_for_macos.sh
+++ Scripts/build_for_macos.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Straight up copied from Carthage
-SANDBOX_FLAGS=
-SWIFTPM_DISABLE_SANDBOX_SHOULD_BE_FLAGGED=$(test -n "${HOMEBREW_SDKROOT}" && echo should_be_flagged)
-if [ "$SWIFTPM_DISABLE_SANDBOX_SHOULD_BE_FLAGGED" = "should_be_flagged" ]; then
-	SANDBOX_FLAGS="--disable-sandbox"
-fi
+# Not needed after all because we don’t use this script to compile in Homebrew
+#SANDBOX_FLAGS=
+#SWIFTPM_DISABLE_SANDBOX_SHOULD_BE_FLAGGED=$(test -n "${HOMEBREW_SDKROOT}" && echo should_be_flagged)
+#if [ "$SWIFTPM_DISABLE_SANDBOX_SHOULD_BE_FLAGGED" = "should_be_flagged" ]; then
+#	SANDBOX_FLAGS="--disable-sandbox"
+#fi
 
 cd "$(dirname "$0")/.."
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/local/opt/openssl/lib/pkgconfig:${HOME}/usr/homebrew/opt/openssl/lib/pkgconfig" swift build $SANDBOX_FLAGS -c release
