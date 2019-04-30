186e942e0818a5f7904783e69e5cd14d9582391b
Frizlab <fload@me.com>
2019-04-30 16:38:58 +0200

    Disable sandbox in macOS compile script when compiling in Homebrew

--- Scripts/build_for_macos.sh
+++ Scripts/build_for_macos.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Straight up copied from Carthage
+SANDBOX_FLAGS=
+SWIFTPM_DISABLE_SANDBOX_SHOULD_BE_FLAGGED=$(test -n "${HOMEBREW_SDKROOT}" && echo should_be_flagged)
+if [ "$SWIFTPM_DISABLE_SANDBOX_SHOULD_BE_FLAGGED" = "should_be_flagged" ]; then
+	SANDBOX_FLAGS="--disable-sandbox"
+fi
+
 cd "$(dirname "$0")/.."
-PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/local/opt/openssl/lib/pkgconfig:${HOME}/usr/homebrew/opt/openssl/lib/pkgconfig" swift build -c release
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/local/opt/openssl/lib/pkgconfig:${HOME}/usr/homebrew/opt/openssl/lib/pkgconfig" swift build $SANDBOX_FLAGS -c release
