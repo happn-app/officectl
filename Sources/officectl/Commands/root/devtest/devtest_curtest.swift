50378b8fe4a046f07eb50b3a1a3fb77ec916cdf4
Frizlab <fload@me.com>
2019-05-10 15:59:59 +0200

    Prepare new test

--- Sources/officectl/Commands/root/devtest/devtest_curtest.swift
+++ Sources/officectl/Commands/root/devtest/devtest_curtest.swift
@@ -19,50 +19,54 @@ func curTest(flags f: Flags, arguments args: [String], context: CommandContext) 
 	let asyncConfig: AsyncConfig = try context.container.make()
 	let officeKitConfig = try context.container.make(OfficeKitConfig.self)
 	
-	let c = try GitHubJWTConnector(key: officeKitConfig.gitHubConfigOrThrow().connectorSettings)
-	let f = c.connect(scope: (), asyncConfig: asyncConfig)
-	.then{ _ -> Future<[GitHubRepository]> in
-		let op = GitHubRepositorySearchOperation(searchedOrganisation: "happn-app", gitHubConnector: c)
-		return asyncConfig.eventLoop.future(from: op, queue: asyncConfig.operationQueue, resultRetriever: { try $0.result.get() })
-	}
-	.then{ repositories -> Future<[FutureResult<[Hook]>]> in
-		let ops = repositories.map{ rep -> AuthenticatedJSONOperation<[Hook]> in
-			var config = URLRequestOperation.Config(request: URLRequest(url: URL(string: "https://api.github.com/repos/" + rep.fullName + "/hooks")!), session: nil)
-			config.acceptableStatusCodes = nil
-			return AuthenticatedJSONOperation<[Hook]>(config: config, authenticator: { request, handler in
-				var request = request
-				request.addValue("Basic THIS_AWESOME_TOKEN", forHTTPHeaderField: "Authorization")
-				handler(.success(request), nil)
-			})
-		}
-		return asyncConfig.eventLoop.executeAll(ops, queue: asyncConfig.operationQueue)
-	}
-	.then{ hooks -> EventLoopFuture<Void> in
-		let hooks = Set(hooks.flatMap{ $0.result ?? [] }.filter{ $0.config.url.absoluteString.contains("email") })
-		let hooksStr = Data(hooks.reduce("", { $0 + $1.url.absoluteString + "\n" }).utf8)
-		_ = try? hooksStr.write(to: URL(fileURLWithPath: "/Users/frizlab/Desktop/toto.txt"))
- 		return asyncConfig.eventLoop.future(())
-	}
+	/* Connect to OpenDirectory */
+	return asyncConfig.eventLoop.newFailedFuture(error: NotImplementedError())
 	
-	struct Hook : Codable, Hashable {
-		
-		var id: Int
-		var url: URL
-		var name: String
-		
-		var config: Config
-		
-		struct Config : Codable, Hashable {
-			var url: URL
-			var contentType: String
-		}
-		
-	}
+	/* List all GitHub project’s hooks */
+//	let c = try GitHubJWTConnector(key: officeKitConfig.gitHubConfigOrThrow().connectorSettings)
+//	let f = c.connect(scope: (), asyncConfig: asyncConfig)
+//	.then{ _ -> Future<[GitHubRepository]> in
+//		let op = GitHubRepositorySearchOperation(searchedOrganisation: "happn-app", gitHubConnector: c)
+//		return asyncConfig.eventLoop.future(from: op, queue: asyncConfig.operationQueue, resultRetriever: { try $0.result.get() })
+//	}
+//	.then{ repositories -> Future<[FutureResult<[Hook]>]> in
+//		let ops = repositories.map{ rep -> AuthenticatedJSONOperation<[Hook]> in
+//			var config = URLRequestOperation.Config(request: URLRequest(url: URL(string: "https://api.github.com/repos/" + rep.fullName + "/hooks")!), session: nil)
+//			config.acceptableStatusCodes = nil
+//			return AuthenticatedJSONOperation<[Hook]>(config: config, authenticator: { request, handler in
+//				var request = request
+//				request.addValue("Basic THIS_AWESOME_TOKEN", forHTTPHeaderField: "Authorization")
+//				handler(.success(request), nil)
+//			})
+//		}
+//		return asyncConfig.eventLoop.executeAll(ops, queue: asyncConfig.operationQueue)
+//	}
+//	.then{ hooks -> EventLoopFuture<Void> in
+//		let hooks = Set(hooks.flatMap{ $0.result ?? [] }.filter{ $0.config.url.absoluteString.contains("email") })
+//		let hooksStr = Data(hooks.reduce("", { $0 + $1.url.absoluteString + "\n" }).utf8)
+//		_ = try? hooksStr.write(to: URL(fileURLWithPath: "/Users/frizlab/Desktop/toto.txt"))
+// 		return asyncConfig.eventLoop.future(())
+//	}
+//
+//	struct Hook : Codable, Hashable {
+//
+//		var id: Int
+//		var url: URL
+//		var name: String
+//
+//		var config: Config
+//
+//		struct Config : Codable, Hashable {
+//			var url: URL
+//			var contentType: String
+//		}
+//
+//	}
+//
+//	return f
 	
-	return f
 	
-	
-	
+	/* Modify Google user test */
 //	let googleConnectorConfig = try officeKitConfig.googleConfigOrThrow().connectorSettings
 //	_ = try nil2throw(googleConnectorConfig.userBehalf, "Google User Behalf")
 //
