597896967eb98a1ce717b7385b19d59f63e6b264
Frizlab <fload@me.com>
2019-07-10 19:05:32 +0200

    Add option to specify path to config file

--- Sources/opendirectory_officectlproxy/main/app.swift
+++ Sources/opendirectory_officectlproxy/main/app.swift
@@ -12,10 +12,17 @@ import Vapor
 
 
 func app(_ env: Environment) throws -> Application {
+	let forcedConfigPath: String?
+	if let idx = env.arguments.lastIndex(where: { $0 == "--config-file" }), idx + 1 < env.arguments.count {
+		forcedConfigPath = env.arguments[idx+1]
+	} else {
+		forcedConfigPath = nil
+	}
+	
 	var config = Config.default()
 	var env = env
 	var services = Services.default()
-	try configure(&config, &env, &services)
+	try configure(&config, &env, &services, forcedConfigPath: forcedConfigPath)
 	
 	let app = try Application(config: config, environment: env, services: services)
 	try boot(app)
--- Sources/opendirectory_officectlproxy/main/configure.swift
+++ Sources/opendirectory_officectlproxy/main/configure.swift
@@ -7,11 +7,15 @@
 
 import Foundation
 
+import OfficeKit
 import Vapor
+import Yaml
 
 
 
-public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services, forcedConfigPath: String?) throws {
+	let conf = try readYamlConfig(forcedConfigFilePath: forcedConfigPath)
+	
 	/* Register routes to the router */
 	let router = EngineRouter.default()
 	try routes(router)
@@ -21,4 +25,30 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 	var middlewares = MiddlewareConfig() /* Create _empty_ middleware config */
 	middlewares.use(ErrorMiddleware.self) /* Catches errors and converts to HTTP response */
 	services.register(middlewares)
+}
+
+
+private func readYamlConfig(forcedConfigFilePath: String?) throws -> (URL, Yaml) {
+	let configURL: URL
+	var isDir: ObjCBool = false
+	let fm = FileManager.default
+	if let path = forcedConfigFilePath {
+		guard fm.fileExists(atPath: path, isDirectory: &isDir), !isDir.boolValue else {
+			throw InvalidArgumentError(message: "Cannot find file at path \(path)")
+		}
+		
+		configURL = URL(fileURLWithPath: path, isDirectory: false)
+	} else {
+		let searchedURLs = [
+			fm.homeDirectoryForCurrentUser.appendingPathComponent(".config/officectl/officectl.yaml", isDirectory: false),
+			URL(fileURLWithPath: "/etc/officectl/officectl.yaml", isDirectory: false)
+		]
+		guard let firstURL = searchedURLs.first(where: { fm.fileExists(atPath: $0.path, isDirectory: &isDir) && !isDir.boolValue }) else {
+			throw MissingFieldError("Config file path")
+		}
+		configURL = firstURL
+	}
+	
+	let configString = try String(contentsOf: configURL, encoding: .utf8)
+	return try (configURL, Yaml.load(configString))
 }
