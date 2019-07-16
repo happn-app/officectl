008552321ab16410c508ff5c4ea2adf8e415e110
Frizlab <fload@me.com>
2019-07-16 23:45:16 +0200

    Fix using the --config-file or --verbose option in the OD proxy

--- Sources/officectl_odproxy/main/app.swift
+++ Sources/officectl_odproxy/main/app.swift
@@ -7,21 +7,34 @@
 
 import Foundation
 
+import OfficeKit
 import Vapor
 
 
 
 func app(_ env: Environment) throws -> Application {
+	var env = env
 	let forcedConfigPath: String?
 	if let idx = env.arguments.lastIndex(where: { $0 == "--config-file" }), idx + 1 < env.arguments.count {
 		forcedConfigPath = env.arguments[idx+1]
+		env.arguments.remove(at: idx)
+		env.arguments.remove(at: idx)
 	} else {
 		forcedConfigPath = nil
 	}
-	let verbose = env.arguments.contains("--verbose")
+	let verbose: Bool
+	if let idx = env.arguments.lastIndex(where: { $0 == "--verbose" }) {
+		verbose = true
+		env.arguments.remove(at: idx)
+	} else {
+		verbose = false
+	}
+	
+	guard !env.arguments.contains(where: { Set(arrayLiteral: "--config-file", "--verbose").contains($0) }) else {
+		throw InvalidArgumentError(message: "The --config-file or --verbose options can only be specified once.")
+	}
 	
 	var config = Config.default()
-	var env = env
 	var services = Services.default()
 	try configure(&config, &env, &services, forcedConfigPath: forcedConfigPath, verbose: verbose)
 	
