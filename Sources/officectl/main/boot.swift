920a20f078f8279992a3dafbaaca1d93f2fe9d69
Frizlab <fload@me.com>
2019-08-16 14:06:02 +0200

    Set OfficeKit logger

--- Sources/officectl/main/boot.swift
+++ Sources/officectl/main/boot.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
+import OfficeKit
 import Vapor
 
 
 
 /** Called after the application has initialized. */
 public func boot(_ app: Application) throws {
+	OfficeKitConfig.logger = try? app.make(Logger.self)
 }
