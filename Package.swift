31772f67b1c17297a460cf3c1b6d7246a25fa6d9
Frizlab <fload@me.com>
2019-10-25 17:19:54 +0200

    Freeze some packages to specific alpha revision (not sure why this is needed)

--- Package.swift
+++ Package.swift
@@ -24,12 +24,12 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-nio.git", from: "2.6.0"),
 		.package(url: "https://github.com/klaas/Guaka.git", .upToNextMinor(from: "0.3.0")),
 //		.package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-		.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha"),
+		.package(url: "https://github.com/vapor/vapor.git", .revision("4.0.0-alpha.3.2")),
 //		.package(url: "https://github.com/vapor/core.git", from: "3.9.0"), /* Async */
 		.package(url: "https://github.com/vapor/open-crypto.git", from: "4.0.0-alpha"),
 //		.package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
 //		.package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
-		.package(url: "https://github.com/vapor/console-kit.git", from: "4.0.0-alpha"), /* Logging */
+		.package(url: "https://github.com/vapor/console-kit.git", .revision("4.0.0-alpha.3")), /* Logging */
 		.package(url: "https://github.com/behrang/YamlSwift.git", from: "3.0.0"),
 		.package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-alpha"),
 		.package(url: "https://github.com/happn-tech/EmailValidator.git", .branch("master")),
