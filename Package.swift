0ad3a72163e5d295e35f8f5548849a8f6c9c27e7
Frizlab <fload@me.com>
2019-10-25 16:50:23 +0200

    Start updating project for Vapor4 beta

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ platformDependentTargets.append(.target(name: "officectl_odproxy", dependencies:
 let package = Package(
 	name: "officectl",
 	platforms: [
-		.macOS(.v10_13)
+		.macOS(.v10_15)
 	],
 	products: [
 		.library(name: "OfficeKit", targets: ["OfficeKit"])
@@ -23,14 +23,14 @@ let package = Package(
 		.package(url: "https://github.com/happn-tech/SemiSingleton.git", from: "2.0.0"),
 		.package(url: "https://github.com/apple/swift-nio.git", from: "2.6.0"),
 		.package(url: "https://github.com/klaas/Guaka.git", .upToNextMinor(from: "0.3.0")),
-//		.package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-		.package(url: "https://github.com/vapor/vapor.git", .revision("4.0.0-alpha.3.2")),
-//		.package(url: "https://github.com/vapor/core.git", from: "3.9.0"), /* Async */
+		/* Leaf is still in 4.0.0-alpha. We need beta to be compatible with Vapor 4.0.0-beta… */
+//		.package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-alpha"),
+		.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
+		/* OpenCrypto is still in alpha */
 		.package(url: "https://github.com/vapor/open-crypto.git", from: "4.0.0-alpha"),
-//		.package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-//		.package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
-		.package(url: "https://github.com/vapor/console-kit.git", .revision("4.0.0-alpha.3")), /* Logging */
+//		.package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
 		.package(url: "https://github.com/behrang/YamlSwift.git", from: "3.0.0"),
+		/* JWT is still in 4.0.0-alpha */
 		.package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-alpha"),
 		.package(url: "https://github.com/happn-tech/EmailValidator.git", .branch("master")),
 		.package(url: "https://github.com/zoul/generic-json-swift.git", from: "1.2.0"),
@@ -48,10 +48,7 @@ let package = Package(
 			/* happn dependencies */
 			"RetryingOperation", "URLRequestOperation", "SemiSingleton", "EmailValidator",
 			/* External dependencies */
-			"NIO", "OpenCrypto"/*, "Service"*/, "ConsoleKit"/*, "Async"*/, "JWTKit", "GenericJSON", "Yaml"
-			/* IMHO this one shouldn’t be needed, but I can’t seem to get rid of it
-			 * in Xcode. Works well with SPM CLI. */
-			,"Vapor"
+			"NIO", "OpenCrypto"/*, "ConsoleKit"*/, "JWTKit", "GenericJSON", "Yaml" ,"Vapor"
 		]),
 		.testTarget(name: "OfficeKitTests", dependencies: ["OfficeKit"]),
 		
