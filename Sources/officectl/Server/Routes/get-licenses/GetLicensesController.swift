10eba3aef8f666d1d74a18f30cf091d3566d3cb2
Frizlab <fload@me.com>
2020-04-09 11:07:25 +0200

    Implement the get MDM devices w/ attributes action

--- Sources/officectl/Server/Routes/get-licenses/GetLicensesController.swift
+++ Sources/officectl/Server/Routes/get-licenses/GetLicensesController.swift
@@ -36,18 +36,56 @@ class GetLicensesController {
 		let user = try authService.logicalUser(fromEmail: getLicensesData.email, servicesProvider: officeKitServiceProvider)
 		
 		let officectlConfig = req.application.officectlConfig
+		let semiSingletonStore = req.application.semiSingletonStore
 		let token = try nil2throw(officectlConfig.tmpSimpleMDMToken)
 		
-		let username = getLicensesData.email.username
+		let emailStr = getLicensesData.email.stringValue
 		
 		return try authService.authenticate(userId: user.userId, challenge: getLicensesData.password, using: req.services)
 		.flatMapThrowing{ authSuccess -> Void in
 			guard authSuccess else {throw "Cannot login with these credentials."}
 		}
-		.flatMapThrowing{ _ -> View in
-			print(username)
+		.flatMap{ _ -> EventLoopFuture<[(SimpleMDMDevice, [String: String])]> in
+			let getDevicesAction: GetMDMDevicesWithAttributesAction = semiSingletonStore.semiSingleton(forKey: token)
+			return getDevicesAction.start(parameters: (), weakeningMode: .always(successDelay: 3600, errorDelay: nil), shouldJoinRunningAction: { _ in true }, shouldRetrievePreviousRun: { _, wasSuccessful in wasSuccessful }, eventLoop: req.eventLoop)
+		}
+		.flatMapThrowing{ devicesAndAttributes -> [[String: String]] in
+			return devicesAndAttributes.compactMap{ deviceAndAttributes -> [[String: String]]? in
+				guard deviceAndAttributes.1["user_email"] == emailStr else {return nil}
+				
+				guard
+					let licensesStr = deviceAndAttributes.1["software_licenses"]?
+						.splitLines()
+						.map({ $0.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) })
+						.filter({ !$0.isEmpty })
+				else {return nil}
+				
+				let jsonDecoder = JSONDecoder()
+				return licensesStr.compactMap{ licenseStr -> [String: String]? in
+					guard let license = try? jsonDecoder.decode(Dictionary<String, String>.self, from: Data(licenseStr.utf8)) else {
+						req.logger.warning("Found invalid license (cannot decode as [String: String]) stored for user \(emailStr) in device \(deviceAndAttributes.0.id)")
+						return nil
+					}
+					return license
+				}
+			}
+			.flatMap{ $0 }
+		}
+		.flatMapThrowing{ licenses -> View in
+			print(licenses)
 			throw NotImplementedError()
 		}
+	}
+	
+}
+
+
+private extension String {
+	
+	func splitLines() -> [String] {
+		var res = [String]()
+		enumerateLines{ line, _ in res.append(line) }
+		return res
 	}
 	
 }
