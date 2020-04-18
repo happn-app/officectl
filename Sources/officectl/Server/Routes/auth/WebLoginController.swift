f78ff101757b854d7a580abb94169858138292f3
Frizlab <fload@me.com>
2020-04-19 00:22:29 +0200

    Implement the “next URL” on the login

--- Sources/officectl/Server/Routes/auth/WebLoginController.swift
+++ Sources/officectl/Server/Routes/auth/WebLoginController.swift
@@ -16,17 +16,25 @@ import Vapor
 class WebLoginController {
 	
 	func showLoginPage(_ req: Request) throws -> EventLoopFuture<View> {
-		let loggedInUser = req.auth.get(LoggedInUser.self)
-		let context = LoginContext(username: loggedInUser?.userId.taggedId.id)
+		struct LoginContext : Encodable {
+			var username: String?
+			var nextURLPath: String?
+		}
+		
+		let context = LoginContext(
+			username: req.auth.get(LoggedInUser.self)?.userId.taggedId.id,
+			nextURLPath: req.headers["Officectl-Login-Next-Page"].last ?? req.query["next"]
+		)
 		return req.view.render("LoginPage", context)
 	}
 	
 	func doLogin(_ req: Request) throws -> Response {
-		return req.redirect(to: "/login", type: .normal)
-	}
-	
-	private struct LoginContext : Encodable {
-		var username: String?
+		struct LoginData : Decodable {
+			var nextURLPath: String?
+		}
+		
+		let loginData = try? req.content.decode(LoginData.self)
+		return req.redirect(to: loginData?.nextURLPath ?? "/", type: .normal)
 	}
 	
 }
