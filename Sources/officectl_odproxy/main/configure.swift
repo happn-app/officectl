790b173449c1bde4d2c2ee91097ce6a3b80658af
Frizlab <fload@me.com>
2020-04-18 09:20:47 +0200

    Move middleware config out of the configure in od_proxy

--- Sources/officectl_odproxy/main/configure.swift
+++ Sources/officectl_odproxy/main/configure.swift
@@ -20,19 +20,7 @@ func configure(_ app: Application, forcedConfigPath: String?, verbose: Bool) thr
 	configureSemiSingleton(verbose)
 	
 	let (url, conf) = try readYamlConfig(forcedConfigFilePath: forcedConfigPath)
-	
 	let serverConfigYaml = try conf.storage(forKey: "server", currentKeyPath: ["Global config"])
-	let serverSecret = try serverConfigYaml.string(forKey: "secret", currentKeyPath: ["Server Config"])
-	
-	let signatureURLPathPrefixTransform: VerifySignatureMiddleware.SignatureURLPathPrefixTransform?
-	if let transformObject = try serverConfigYaml.optionalNonNullStorage(forKey: "signature_url_path_prefix_transform", currentKeyPath: ["Server Config"]) {
-		signatureURLPathPrefixTransform = (
-			from: try transformObject.string(forKey: "from", currentKeyPath: ["Signature URL Prefix Transform"]),
-			to:   try transformObject.string(forKey: "to",   currentKeyPath: ["Signature URL Prefix Transform"])
-		)
-	} else {
-		signatureURLPathPrefixTransform = nil
-	}
 	
 	/* Register the global config */
 	app.globalConfig = try GlobalConfig(genericConfig: conf, pathsRelativeTo: url)
@@ -53,23 +41,7 @@ func configure(_ app: Application, forcedConfigPath: String?, verbose: Bool) thr
 	let openDirectoryServiceConfigYaml = try conf.storage(forKey: "open_directory_config", currentKeyPath: ["Global config"])
 	app.openDirectoryServiceConfig = try OpenDirectoryServiceConfig(providerId: OpenDirectoryService.providerId, serviceId: "_internal_od_", serviceName: "Internal Open Directory Service", mergePriority: nil, keyedConfig: openDirectoryServiceConfigYaml, pathsRelativeTo: url)
 	
-	/* Register middleware */
-	app.middleware.use(ErrorMiddleware(handleError)) /* Catches errors and converts them to HTTP response */
-	app.middleware.use(VerifySignatureMiddleware(secret: Data(serverSecret.utf8), signatureURLPathPrefixTransform: signatureURLPathPrefixTransform))
-	
-	try routes(app)
-}
-
-
-private func handleError(req: Request, error: Error) -> Response {
-	do {
-		#warning("TODO: Log the error")
-		return try ApiResponse<String>(error: error).syncEncode(for: req)
-	} catch {
-		var headers = HTTPHeaders()
-		headers.replaceOrAdd(name: .contentType, value: "application/json")
-		return Response(status: .internalServerError, headers: headers, body: Response.Body(string: #"{"error":{"domain":"top","code":42,"message":"Cannot even encode the upstream error…"}}"#))
-	}
+	try routes_and_middlewares(app, serverConfigYaml)
 }
 
 
