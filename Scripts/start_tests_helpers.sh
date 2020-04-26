eea2ab7acb1f7fec175035ff2127a47358c8976a
Frizlab <fload@me.com>
2020-04-26 10:40:00 +0200

    Enhance the LDAP for test docker container creation

--- Scripts/start_tests_helpers.sh
+++ Scripts/start_tests_helpers.sh
@@ -13,8 +13,10 @@ docker rm -f "$LDAP_NAME" || true
 echo "Removing previous LDAP data"
 rm -frv "$LDAP_DATA_PATH"
 echo "Starting new LDAP container"
-docker run -d --name "$LDAP_NAME" --volume "$LDAP_INIT_PATH":/etc/ldap.dist/prepopulate --volume "$LDAP_DATA_PATH"/config:/etc/ldap --volume "$LDAP_DATA_PATH"/data:/var/lib/ldap -e SLAPD_PASSWORD=toto -e SLAPD_DOMAIN=happn.test -p8389:389 dinkel/openldap
+docker network create officectl 2>/dev/null || true
+docker run -d --net officectl --name "$LDAP_NAME" --volume "$LDAP_INIT_PATH":/etc/ldap.dist/prepopulate --volume "$LDAP_DATA_PATH"/config:/etc/ldap --volume "$LDAP_DATA_PATH"/data:/var/lib/ldap -e SLAPD_PASSWORD=toto -e SLAPD_DOMAIN=happn.test -p8389:389 dinkel/openldap
 
 echo
 echo "Done. LDAP should be accessible with:"
 echo "   ldapsearch -p 8389 -h localhost -D cn=admin,dc=happn,dc=test -w toto -b dc=happn,dc=test"
+echo "You should be able access the LDAP from within a docker container using the “--net officectl” option, with hostname “${LDAP_NAME}” on port 389."
