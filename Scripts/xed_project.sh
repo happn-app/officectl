a45a7cd778e77c6b4f1bc00093b99459e2f2c5c7
Frizlab <fload@me.com>
2021-07-21 01:12:54 +0200

    Remove obsolete scripts

--- Scripts/xed_project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"/..
-ps aux | grep -q '[X]code\.app/Contents/MacOS/Xcode' && { echo "Xcode must not be launched" >/dev/stderr; exit 1; }
-
-# The explicit PKG_CONFIG_PATH for openssl is required when using Xcode’s SPM,
-# contrary to CLI’s SPM (because CLI’s SPM is aware of brew).
-PKG_CONFIG_PATH="$PKG_CONFIG_PATH:`brew --prefix openssl@1.1`/lib/pkgconfig" xed .
