58f0448c454dedc174bd03d2502eb9528e31b9e9
Frizlab <fload@me.com>
2021-12-03 18:17:28 +0100

    Migrate UsersController to async

--- Sources/officectl/Server/Routes/api/UsersController.swift
+++ Sources/officectl/Server/Routes/api/UsersController.swift
@@ -17,14 +17,14 @@ import Vapor
 
 class UsersController {
 	
-	func getAllUsers(_ req: Request) throws -> EventLoopFuture<ApiResponse<ApiUsersSearchResult>> {
+	func getAllUsers(_ req: Request) async throws -> ApiResponse<ApiUsersSearchResult> {
 		let sProvider = req.application.officeKitServiceProvider
 		
 		let serviceIdsStr: String? = req.query["service_ids"]
 		let serviceIds = serviceIdsStr?.split(separator: ",").map(String.init)
 		let services = try Set(serviceIds.flatMap{ try $0.map{ try sProvider.getUserDirectoryService(id: $0) } } ?? Array(sProvider.getAllUserDirectoryServices()))
 		
-		return try MultiServicesUser.fetchAll(in: services, using: req.services).flatMapThrowing{
+		return try await MultiServicesUser.fetchAll(in: services, using: req.services).flatMapThrowing{
 			let (users, fetchErrorsByService) = $0
 			let fetchApiErrorsByService = fetchErrorsByService.mapValues{ ApiError(error: $0, environment: req.application.environment) }
 			let orderedServices = try req.application.officeKitConfig.orderedServiceConfigs.map{ try sProvider.getUserDirectoryService(id: $0.serviceId) }
@@ -32,27 +32,28 @@ class UsersController {
 				try ApiUser(multiUsers: $0, orderedServices: orderedServices)
 			}.sorted{ ($0.lastName ?? "").localizedCompare($1.lastName ?? "") != .orderedDescending }))
 		}
+		.get()
 	}
 	
-	func getMe(_ req: Request) throws -> EventLoopFuture<ApiResponse<ApiUserSearchResult>> {
+	func getMe(_ req: Request) async throws -> ApiResponse<ApiUserSearchResult> {
 		let loggedInUser = try req.auth.require(LoggedInUser.self)
-		return try getUserNoAuthCheck(userId: loggedInUser.user.dsuIdPair, request: req)
+		return try await getUserNoAuthCheck(userId: loggedInUser.user.dsuIdPair, request: req)
 	}
 	
-	func getUser(_ req: Request) throws -> EventLoopFuture<ApiResponse<ApiUserSearchResult>> {
+	func getUser(_ req: Request) async throws -> ApiResponse<ApiUserSearchResult> {
 		let loggedInUser = try req.auth.require(LoggedInUser.self)
 		let fetchedUserId = try AnyDSUIdPair.getAsParameter(named: "dsuid-pair", from: req)
 		guard try loggedInUser.isAdmin || loggedInUser.representsSameUserAs(dsuIdPair: fetchedUserId, request: req) else {
 			throw Abort(.forbidden, reason: "Non-admin users can only see themselves.")
 		}
 		
-		return try getUserNoAuthCheck(userId: fetchedUserId, request: req)
+		return try await getUserNoAuthCheck(userId: fetchedUserId, request: req)
 	}
 	
-	private func getUserNoAuthCheck(userId: AnyDSUIdPair, request: Request) throws -> EventLoopFuture<ApiResponse<ApiUserSearchResult>> {
+	private func getUserNoAuthCheck(userId: AnyDSUIdPair, request: Request) async throws -> ApiResponse<ApiUserSearchResult> {
 		let officeKitConfig = request.application.officeKitConfig
 		let sProvider = request.application.officeKitServiceProvider
-		return try MultiServicesUser.fetch(from: userId, in: sProvider.getAllUserDirectoryServices(), using: request.services)
+		return try await MultiServicesUser.fetch(from: userId, in: sProvider.getAllUserDirectoryServices(), using: request.services)
 		.flatMapThrowing{ multiUser in
 			let orderedServices = try officeKitConfig.orderedServiceConfigs.map{ try sProvider.getUserDirectoryService(id: $0.serviceId) }
 			return try ApiResponse.data(
@@ -63,6 +64,7 @@ class UsersController {
 				)
 			)
 		}
+		.get()
 	}
 	
 }
