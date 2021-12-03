b24df7ab3ef654c316273a6eb9614a2228abf6f1
Frizlab <fload@me.com>
2021-12-03 17:53:12 +0100

    Migrate UserCredsAuthenticator to async

--- Sources/officectl/Server/Auth/UserCredsAuthenticator.swift
+++ Sources/officectl/Server/Auth/UserCredsAuthenticator.swift
@@ -12,7 +12,7 @@ import Vapor
 
 
 
-struct UserCredsAuthenticator : CredentialsAuthenticator {
+struct UserCredsAuthenticator : AsyncCredentialsAuthenticator {
 	
 	typealias Credentials = LoginData
 	
@@ -36,38 +36,35 @@ struct UserCredsAuthenticator : CredentialsAuthenticator {
 		usernameType = ut
 	}
 	
-	func authenticate(credentials loginData: LoginData, for request: Request) -> EventLoopFuture<Void> {
-		return request.eventLoop.future()
-		.flatMapThrowing{
-			let sProvider = request.application.officeKitServiceProvider
-			let authService = try sProvider.getDirectoryAuthenticatorService()
-			
-			let user: AnyDSUPair
-			switch self.usernameType {
-			case .email:    user = try AnyDSUPair(service: authService, user: authService.logicalUser(fromEmail: nil2throw(Email(string: loginData.username), "Invalid email"), servicesProvider: sProvider))
-			case .taggedId: user = try AnyDSUIdPair(string: loginData.username, servicesProvider: sProvider).dsuPair()
-			}
-			guard user.service.config.serviceId == authService.config.serviceId else {
-				throw Abort(.forbidden, reason: "Tried to login with an id which is not from the auth service (expected \(authService.config.serviceId)).")
-			}
-			
-			return try authService.authenticate(userId: user.user.userId, challenge: loginData.password, using: request.services)
-			.flatMapThrowing{ authSuccess -> Void in
-				guard authSuccess else {throw Abort(.forbidden, reason: "Invalid credentials. Please check your username and password.")}
-				return ()
-			}
-			.flatMapThrowing{ _ -> EventLoopFuture<Bool> in
-				return try authService.validateAdminStatus(userId: user.user.userId, using: request.services)
-			}
-			.flatMap{ $0 }
-			.flatMapThrowing{ isAdmin in
-				/* The password of the user is verified and we have its admin
-				 * status. Let’s log it in. */
-				request.auth.login(LoggedInUser(user: user, isAdmin: isAdmin))
-				return ()
-			}
+	func authenticate(credentials loginData: LoginData, for request: Request) async throws {
+		let sProvider = request.application.officeKitServiceProvider
+		let authService = try sProvider.getDirectoryAuthenticatorService()
+		
+		let user: AnyDSUPair
+		switch self.usernameType {
+		case .email:    user = try AnyDSUPair(service: authService, user: authService.logicalUser(fromEmail: nil2throw(Email(string: loginData.username), "Invalid email"), servicesProvider: sProvider))
+		case .taggedId: user = try AnyDSUIdPair(string: loginData.username, servicesProvider: sProvider).dsuPair()
+		}
+		guard user.service.config.serviceId == authService.config.serviceId else {
+			throw Abort(.forbidden, reason: "Tried to login with an id which is not from the auth service (expected \(authService.config.serviceId)).")
+		}
+		
+		return try await authService.authenticate(userId: user.user.userId, challenge: loginData.password, using: request.services)
+		.flatMapThrowing{ authSuccess -> Void in
+			guard authSuccess else {throw Abort(.forbidden, reason: "Invalid credentials. Please check your username and password.")}
+			return ()
+		}
+		.flatMapThrowing{ _ -> EventLoopFuture<Bool> in
+			return try authService.validateAdminStatus(userId: user.user.userId, using: request.services)
 		}
 		.flatMap{ $0 }
+		.flatMapThrowing{ isAdmin in
+			/* The password of the user is verified and we have its admin
+			 * status. Let’s log it in. */
+			request.auth.login(LoggedInUser(user: user, isAdmin: isAdmin))
+			return ()
+		}
+		.get()
 	}
 	
 }
