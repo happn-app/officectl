c12f94c4cf4e968553a25336bc3f3c02c1f6caf4
Frizlab <fload@me.com>
2021-12-03 17:52:06 +0100

    Migrate UserBearerAuthenticator to async

--- Sources/officectl/Server/Auth/UserBearerAuthenticator.swift
+++ Sources/officectl/Server/Auth/UserBearerAuthenticator.swift
@@ -13,17 +13,14 @@ import Vapor
 
 
 
-struct UserBearerAuthenticator : BearerAuthenticator {
+struct UserBearerAuthenticator : AsyncBearerAuthenticator {
 	
-	func authenticate(bearer: BearerAuthorization, for request: Request) -> EventLoopFuture<Void> {
-		return request.eventLoop.future()
-		.flatMapThrowing{
-			let sProvider = request.application.officeKitServiceProvider
-			let jwtSecret = try nil2throw(request.application.officectlConfig.serverConfig?.jwtSecret)
-			let token: ApiAuth.Token = try JWTSigner.hs256(key: jwtSecret).verify(bearer.token)
-			try request.auth.login(LoggedInUser(user: AnyDSUIdPair(taggedId: token.sub, servicesProvider: sProvider).dsuPair(), isAdmin: token.adm))
-			/* Note: We do **not** verify the user exists or is admin. We could. But we assume the JWT expiring very fast we won’t have a problem. */
-		}
+	func authenticate(bearer: BearerAuthorization, for request: Request) async throws {
+		let sProvider = request.application.officeKitServiceProvider
+		let jwtSecret = try nil2throw(request.application.officectlConfig.serverConfig?.jwtSecret)
+		let token: ApiAuth.Token = try JWTSigner.hs256(key: jwtSecret).verify(bearer.token)
+		try request.auth.login(LoggedInUser(user: AnyDSUIdPair(taggedId: token.sub, servicesProvider: sProvider).dsuPair(), isAdmin: token.adm))
+		/* Note: We do **not** verify the user exists or is admin. We could. But we assume the JWT expiring very fast we won’t have a problem. */
 	}
 	
 }
