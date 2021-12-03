a08706325312749d9d134b26390f9759ccae4bc2
Frizlab <fload@me.com>
2021-12-03 17:54:47 +0100

    Migrate UserSessionAuthenticator to async

--- Sources/officectl/Server/Auth/UserSessionAuthenticator.swift
+++ Sources/officectl/Server/Auth/UserSessionAuthenticator.swift
@@ -13,32 +13,29 @@ import Vapor
 
 
 
-struct UserSessionAuthenticator : SessionAuthenticator {
+struct UserSessionAuthenticator : AsyncSessionAuthenticator {
 	
 	typealias User = LoggedInUser
 	
-	func authenticate(sessionID: TaggedId, for request: Request) -> EventLoopFuture<Void> {
-		return request.eventLoop.future()
-		.flatMapThrowing{
-			/* Let’s verify the user still exists and whether it is an admin */
-			let sProvider = request.application.officeKitServiceProvider
-			let authService = try sProvider.getDirectoryAuthenticatorService()
-			let erasedUserId = try authService.userId(fromString: sessionID.id)
-			
-			guard sessionID.tag == authService.config.serviceId else {
-				throw Abort(.forbidden, reason: "It seems your beloved admins changed the auth service of officectl. Don’t worry you don’t have to know what that means; simply login again. :)")
-			}
-			
-			let futureUser = try authService.existingUser(fromUserId: erasedUserId, propertiesToFetch: [.identifyingEmail], using: request.services)
-				.unwrap(or: Abort(.forbidden, reason: "The logged in user seems to have been deleted"))
-			let futureIsAdmin = try authService.validateAdminStatus(userId: erasedUserId, using: request.services)
-			
-			return futureUser.and(futureIsAdmin).map{ userAndIsAdmin in
-				let (user, isAdmin) = userAndIsAdmin
-				request.auth.login(LoggedInUser(user: AnyDSUPair(service: authService, user: user), isAdmin: isAdmin))
-			}
+	func authenticate(sessionID: TaggedId, for request: Request) async throws {
+		/* Let’s verify the user still exists and whether it is an admin */
+		let sProvider = request.application.officeKitServiceProvider
+		let authService = try sProvider.getDirectoryAuthenticatorService()
+		let erasedUserId = try authService.userId(fromString: sessionID.id)
+		
+		guard sessionID.tag == authService.config.serviceId else {
+			throw Abort(.forbidden, reason: "It seems your beloved admins changed the auth service of officectl. Don’t worry you don’t have to know what that means; simply login again. :)")
 		}
-		.flatMap{ $0 }
+		
+		let futureUser = try authService.existingUser(fromUserId: erasedUserId, propertiesToFetch: [.identifyingEmail], using: request.services)
+			.unwrap(or: Abort(.forbidden, reason: "The logged in user seems to have been deleted"))
+		let futureIsAdmin = try authService.validateAdminStatus(userId: erasedUserId, using: request.services)
+		
+		return try await futureUser.and(futureIsAdmin).map{ userAndIsAdmin in
+			let (user, isAdmin) = userAndIsAdmin
+			request.auth.login(LoggedInUser(user: AnyDSUPair(service: authService, user: user), isAdmin: isAdmin))
+		}
+		.get()
 	}
 	
 }
