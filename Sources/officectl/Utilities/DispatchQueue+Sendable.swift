c423aa4f59b152275993bd864eee23f40bcfee42
Frizlab <fload@me.com>
2022-09-29 15:49:44 +0200

    Make DispatchQueue @unchecked Sendable for convenience

---
+++ Sources/officectl/Utilities/DispatchQueue+Sendable.swift
@@ -0,0 +1,13 @@
+/*
+ * DispatchQueue+Sendable.swift
+ * officectl
+ *
+ * Created by François Lamboley on 2022/09/26.
+ */
+
+import Foundation
+
+
+
+/* Using same rationale as OperationQueue: https://forums.swift.org/t/sendable-in-foundation/59577 */
+extension DispatchQueue : @unchecked Sendable {}
