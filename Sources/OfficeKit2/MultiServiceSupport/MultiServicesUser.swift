33629b6db9e14997e6ad836caad6950fb598b998
Frizlab <fload@me.com>
2022-10-31 21:11:40 +0100

    Add merge method in MultiServicesUser
    The method compile, but I’m not sure it works…

--- Sources/OfficeKit2/MultiServiceSupport/MultiServicesUser.swift
+++ Sources/OfficeKit2/MultiServiceSupport/MultiServicesUser.swift
@@ -103,4 +103,143 @@ public extension MultiServicesUser {
 		return await fetchStep(from: userAndService, in: services)
 	}
 	
+//	static func fetchAll(in services: Set<HashableUserService>, using depServices: Services) async throws -> (users: [MultiServicesUser], fetchErrorsByServices: [HashableUserService: Error]) {
+//		let (pairs, fetchErrorsByService) = await HashableUserAndService.fetchAll(in: services, using: depServices)
+//		let validServices = services.subtracting(fetchErrorsByService.keys)
+//		return try await (MultiServicesUser.merge(dsuPairs: Set(pairs), validServices: validServices), fetchErrorsByService)
+//	}
+	
+	/**
+	 Try and merge all the given users in a collection of multi-services users.
+	 
+	 All the returned users will have a User for all the valid services IDs (the value being `nil` if a linked user was not found for a given user for the given service).
+	 If the valid services IDs are set to `nil`, they are inferred from the set of user and services.
+	 
+	 If the `allowNonValidServices` arg is set to `true`, the returned users might contain a User for a service that has not been declared valid.
+	 (The argument is only useful when `validServices` is set to a non-`nil` value.)
+	 
+	 - Note: The method is async though everything in it is synchronous because the computation can be long and we want not to block everything while the computation is going on.
+	 Maybe we should check with absolute certainty the function will actually be called in the bg, but from my limited testing it seems it should be. */
+	static func merge(userAndServices: Set<HashableUserAndService>, validServices: Set<HashableUserService>? = nil, allowNonValidServices: Bool = false) async throws -> [MultiServicesUser] {
+		/* Transform the input to get something we can use (DSUPairs to LinkedUsers + extracting the list of services). */
+		let services: Set<HashableUserService>
+		let linkedUsersByUserAndService: [HashableUserAndService: LinkedUser]
+		do {
+			var servicesBuilding = Set<HashableUserService>()
+			var linkedUsersByUserAndServiceBuilding = [HashableUserAndService: LinkedUser](minimumCapacity: userAndServices.count)
+			for userAndService in userAndServices {
+				assert(linkedUsersByUserAndServiceBuilding[userAndService] == nil)
+				linkedUsersByUserAndServiceBuilding[userAndService] = LinkedUser(userAndService: userAndService)
+				servicesBuilding.insert(.init(userAndService.value.service))
+			}
+			linkedUsersByUserAndService = linkedUsersByUserAndServiceBuilding
+			services = servicesBuilding
+		}
+		let validServices = validServices ?? services
+		
+		/* Compute relations between the users. */
+		for (_, linkedUser) in linkedUsersByUserAndService {
+			let currentUserServiceID = linkedUser.userAndService.value.serviceID
+			for service in services {
+				let serviceID = service.value.id
+				guard serviceID != currentUserServiceID else {continue}
+				guard let logicallyLinkedPair = try? UserAndServiceFrom(user: service.value.logicalUser(fromWrappedUser: linkedUser.userAndService.value.wrappedUser), service: service.value)!  else {
+//					OfficeKitConfig.logger?.debug("Error finding logically linked user with: {\n  source service ID: \(currentUserServiceID)\n  dest service ID:\(serviceID)\n  source user pair: \(linkedUser.userAndService)\n}")
+					continue
+				}
+				guard let logicallyLinkedLinkedUser = linkedUsersByUserAndService[.init(logicallyLinkedPair)] else {
+//					OfficeKitConfig.logger?.debug("Found logically linked user, but user does not exist: {\n  source service ID: \(currentUserServiceID)\n  dest service ID:\(serviceID)\n  source user pair: \(linkedUser.userAndService)\n  dest user pair: \(logicallyLinkedPair.dsuIDPair)\n}")
+					continue
+				}
+				try linkedUser.link(to: logicallyLinkedLinkedUser)
+			}
+		}
+		
+		/* Merge the linked users in MultiServicesUsers. */
+		var treatedUserAndServices = Set<HashableUserAndService>()
+		return try linkedUsersByUserAndService.compactMap{ kv -> MultiServicesUser? in
+			let (userAndService, linkedUser) = kv
+			
+			guard !treatedUserAndServices.contains(userAndService) else {return nil}
+			treatedUserAndServices.insert(userAndService)
+			
+			guard allowNonValidServices || validServices.contains(.init(userAndService.value.service)) else {
+				OfficeKitConfig.logger?.info("Not adding UserAndService \(userAndService) in multi-user because it doesn’t have an explicitly-declared-valid service")
+				return nil
+			}
+			
+			var res: [HashableUserService: (any User)?] = [.init(linkedUser.userAndService.value.service): linkedUser.userAndService.value.user]
+			for subLinkedUser in linkedUser.linkedUserByService.values {
+				guard !treatedUserAndServices.contains(subLinkedUser.userAndService) else {
+					throw InternalError(message: "Got already treated linked user! \(subLinkedUser.userAndService) for \(userAndService)")
+				}
+				guard res[.init(subLinkedUser.userAndService.value.service)] == nil else {
+					throw InternalError(message: "Got two users for service ID \(subLinkedUser.userAndService.value.serviceID): \(res[.init(subLinkedUser.userAndService.value.service)]!!) and \(subLinkedUser.userAndService)")
+				}
+				res[.init(subLinkedUser.userAndService.value.service)] = subLinkedUser.userAndService.value.user
+				treatedUserAndServices.insert(subLinkedUser.userAndService)
+			}
+			/* Setting a value for all valid services IDs */
+			for s in validServices {
+				guard res[s] == nil else {continue}
+				res[s] = .some(nil)
+			}
+			return res.mapValues{ .success($0) }
+		}
+	}
+	
+}
+
+
+private class LinkedUser : CustomStringConvertible {
+	
+	let userAndService: HashableUserAndService
+	var linkedUserByService: [HashableUserService: LinkedUser] = [:]
+	
+	var description: String {
+		return "LinkedUser<\("service.config.serviceID") - \("user")>; linkedUsers: \("linkedUserByServiceID.keys")"
+	}
+	
+	init(userAndService: HashableUserAndService) {
+		self.userAndService = userAndService
+	}
+	
+	func link(to linkedUser: LinkedUser) throws {
+		var visited = Set<[HashableUserAndService]>()
+		return try link(to: linkedUser, visited: &visited)
+	}
+	
+	private func link(to linkedUser: LinkedUser, visited: inout Set<[HashableUserAndService]>) throws {
+		guard !visited.contains([userAndService, linkedUser.userAndService]) else {return}
+		visited.insert([userAndService, linkedUser.userAndService])
+		
+		guard userAndService != linkedUser.userAndService else {
+			/* Not linking myself to myself… */
+			return
+		}
+		
+		/* Make the actual link. */
+		if let currentlyLinkedUser = linkedUserByService[.init(linkedUser.userAndService.value.service)] {
+			guard currentlyLinkedUser.userAndService == linkedUser.userAndService else {
+				throw InternalError(message: "UserAndService \(userAndService) is asked to be linked to \(linkedUser.userAndService), but is also already linked to \(currentlyLinkedUser.userAndService)")
+			}
+		} else {
+			linkedUserByService[linkedUser.userAndService.value.service] = linkedUser
+		}
+		/* Make the reverse link. */
+		try linkedUser.link(to: self, visited: &visited)
+		/* Link related users. */
+		for toLink in linkedUserByService.values {
+			assert(toLink.linkedUserByService.values.contains(where: { $0.userAndService == userAndService }))
+			try toLink.link(to: linkedUser, visited: &visited)
+		}
+	}
+	
+}
+
+
+private struct InternalError : Error {
+	
+	var message: String
+	
 }
