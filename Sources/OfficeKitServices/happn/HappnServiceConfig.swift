c2e389e3db0677d1e218510a75655e0217fb92fe
Frizlab <fload@me.com>
2022-11-18 20:22:15 +0100

    Remove useless property from happn service config

--- Sources/OfficeKitServices/happn/HappnServiceConfig.swift
+++ Sources/OfficeKitServices/happn/HappnServiceConfig.swift
@@ -14,16 +14,12 @@ import GenericJSON
 
 public struct HappnServiceConfig : Sendable, Codable {
 	
-	/* We set a random default value because we don’t want this to be part of the Codable representation,
-	 *  but we don’t want an invalid id by default either. */
-	public var serviceID: String = UUID().uuidString
 	public var serviceName: String
 	public var connectorSettings: ConnectorSettings
 	
-	init(serviceID: String, json: JSON) throws {
+	init(json: JSON) throws {
 		let data = try JSONEncoder().encode(json)
 		self = try JSONDecoder().decode(Self.self, from: data)
-		self.serviceID = serviceID
 	}
 	
 	public struct ConnectorSettings : Sendable, Codable {
