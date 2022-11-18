e9a3e29963c841dc7ac882cad977d91e84cb2856
Frizlab <fload@me.com>
2022-11-18 21:20:43 +0100

    Add AuthRequestProcessor in OfficeKit2 public utils

---
+++ Sources/OfficeKit2/Utilities/Public/AuthRequestProcessor.swift
@@ -0,0 +1,35 @@
+/*
+ * AuthRequestProcessor.swift
+ * OfficeKit
+ *
+ * Created by François Lamboley on 2022/11/18.
+ */
+
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+import APIConnectionProtocols
+import TaskQueue
+import URLRequestOperation
+
+
+
+public struct AuthRequestProcessor : RequestProcessor {
+	
+	public let authHandler: (URLRequest, @escaping (Result<URLRequest, Error>) -> Void) -> Void
+	
+	public init(authHandler: @escaping (URLRequest) throws -> URLRequest) {
+		self.authHandler = { req, handler in handler(Result{ try authHandler(req) }) }
+	}
+	
+	public init<Auth : Authenticator & HasTaskQueue>(_ auth: Auth) where Auth.Request == URLRequest {
+		authHandler = { req, handler in Task{ handler(await Result{ try await auth.authenticate(request: req) }) } }
+	}
+	
+	public func transform(urlRequest: URLRequest, handler: @escaping (Result<URLRequest, Error>) -> Void) {
+		authHandler(urlRequest, handler)
+	}
+	
+}
