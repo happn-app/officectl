f872e2da932d1795d9c9d2c77cc2ef6150dc9112
Frizlab <fload@me.com>
2022-11-18 19:41:48 +0100

    Implement happn service config

--- Sources/OfficeKitServices/happn/HappnServiceConfig.swift
+++ Sources/OfficeKitServices/happn/HappnServiceConfig.swift
@@ -12,11 +12,42 @@ import GenericJSON
 
 
 
-public struct HappnServiceConfig : Sendable {
+public struct HappnServiceConfig : Sendable, Codable {
 	
-//	public var connectorSettings: HappnConnector.Settings
+	/* We set a random default value because we don’t want this to be part of the Codable representation,
+	 *  but we don’t want an invalid id by default either. */
+	public var serviceID: String = UUID().uuidString
+	public var serviceName: String
+	public var connectorSettings: ConnectorSettings
 	
-	init(json: JSON) throws {
+	init(serviceID: String, json: JSON) throws {
+		let data = try JSONEncoder().encode(json)
+		self = try JSONDecoder().decode(Self.self, from: data)
+		self.serviceID = serviceID
+	}
+	
+	public struct ConnectorSettings : Sendable, Codable {
+		
+		public var clientID: String
+		public var clientSecret: String
+		
+		public var username: String
+		public var password: String
+		
+		private enum CodingKeys : String, CodingKey {
+			
+			case clientID = "client_id", clientSecret = "client_secret"
+			case username, password
+			
+		}
+		
+	}
+	
+	private enum CodingKeys : String, CodingKey {
+		
+		case serviceName = "service_id"
+		case connectorSettings = "connector_settings"
+		
 	}
 	
 }
