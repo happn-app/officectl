c273a2c413d0c05e6a729354a1b2709a1247877a
Frizlab <fload@me.com>
2023-01-02 22:53:47 +0100

    Add some tests for user utils

---
+++ Tests/OfficeKit2-Tests/UserUtilsTests.swift
@@ -0,0 +1,47 @@
+/*
+ * UserUtilsTests.swift
+ * OfficeKitTests
+ *
+ * Created by François Lamboley on 2023/01/02.
+ */
+
+import Foundation
+import XCTest
+
+@testable import OfficeKit2
+
+
+
+final class UserUtilsTests : XCTestCase {
+	
+	func testSetValueIfNeeded() throws {
+		var value: String = "no"
+		_ = DummyUser1.setValueIfNeeded("yes" as Any, in: &value, converter: { $0 as? String })
+		XCTAssertEqual(value, "yes")
+	}
+	
+	func testSetValueIfNeededOptional() throws {
+		var value: String = "no"
+		_ = DummyUser1.setValueIfNeeded("yes" as Any?, in: &value, converter: { $0 as? String })
+		XCTAssertEqual(value, "yes")
+	}
+	
+	func testSetValueIfNeededOptionalInvalidType() throws {
+		var value: String? = "no"
+		_ = DummyUser1.setValueIfNeeded(12 as Any, in: &value, converter: { $0 as? String })
+		XCTAssertEqual(value, "no")
+	}
+	
+	func testSetValueIfNeededOptionalNilValue() throws {
+		var value: String? = "no"
+		_ = DummyUser1.setValueIfNeeded(nil as Any?, in: &value, converter: { $0 as? String })
+		XCTAssertEqual(value, nil)
+	}
+	
+	func testSetValueIfNeededOptionalHiddenNilValue() throws {
+		var value: String? = "no"
+		_ = DummyUser1.setValueIfNeeded(Optional<Int>.none as Any, in: &value, converter: { $0 as? String })
+		XCTAssertEqual(value, nil)
+	}
+	
+}
