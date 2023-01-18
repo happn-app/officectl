38c71883399dca923198f8ecfc04d6b184afedb0
Frizlab <fload@me.com>
2023-01-18 15:57:50 +0100

    Add happn schema in test LDAP

---
+++ Scripts/generate-happn-schema-ldif.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# Move in the folder with the schema(s)
+cd "$(dirname "$0")"/../TestsData/confs/shared/ldap-docker-init/schemas
+
+TMP_CONF_FILE="_ldap.conf"
+
+# Verify the conf file does not exist.
+test ! -e "$TMP_CONF_FILE"
+
+# Create the temporary conf file that will be used to do the conversion of the .schema files into ldif files.
+for schema in *.schema; do
+	echo "include $schema" >>"$TMP_CONF_FILE"
+done
+
+slaptest -f "$TMP_CONF_FILE" -F .
+rm -f "$TMP_CONF_FILE"
+
+# Move the generated ldifs in .
+for ldif in "./cn=config/cn=schema"/*.ldif; do
+	mv -f "$ldif" "$(basename "$ldif" | sed -E 's/^cn=\{0\}//')"
+done
+
+# Remove extra files
+rm -fr "./cn=config.ldif"
+rm -fr "./cn=config"
+
+# Print some info
+echo "The ldif have been created. Only the following properties should be kept (not removed automatically):"
+echo "  - dn (value should start with “cn={0}”);"
+echo "  - objectClass (value should be “olcSchemaConfig”);"
+echo "  - cn (value should be the value of the dn minus “cn=”);"
+echo "  - olcAttributeTypes"
+echo "  - olcObjectClasses"
+echo
+echo "The dn and cn should be modified: remove “{0}” for both keys, and add “,cn=schema,cn=config” to dn."
+echo
+echo "Source: https://www.cyrill-gremaud.ch/how-to-add-new-schema-to-openldap-2-4/"
--- Scripts/start-tests-helpers.sh
+++ Scripts/start-tests-helpers.sh
@@ -7,7 +7,8 @@ readonly LDAP_IMAGE="bitnami/openldap:2.6.3"
 readonly LDAP_CONTAINER_NAME="officectl_testhelper-ldap"
 
 readonly LDAP_DATA_PATH="$(pwd)/TestsData/live/docker/ldap"
-readonly LDAP_INIT_PATH="$(pwd)/TestsData/confs/shared/ldap-docker-init"
+readonly LDAP_INIT_PATH="$(pwd)/TestsData/confs/shared/ldap-docker-init/ldifs"
+readonly LDAP_SCHEMAS_PATH="$(pwd)/TestsData/confs/shared/ldap-docker-init/schemas"
 
 echo "*** Stopping and deleting previous LDAP container if applicable..."
 docker rm -f "$LDAP_CONTAINER_NAME" || true
@@ -18,10 +19,12 @@ mkdir -p "$LDAP_DATA_PATH"
 echo "*** Starting new LDAP container..."
 docker network create officectl 2>/dev/null || true
 #	-e LDAP_LOGLEVEL="-1"                          \+
-docker run --detach                               \
+#docker run --rm -it                              \+
+ docker run --detach                              \
 	--name "$LDAP_CONTAINER_NAME"                  \
 	--net officectl                                \
 	--volume "$LDAP_INIT_PATH":"/ldifs"            \
+	--volume "$LDAP_SCHEMAS_PATH":"/schemas"       \
 	--volume "$LDAP_DATA_PATH":"/bitnami/openldap" \
 	-e LDAP_ROOT="dc=happn,dc=test"                \
 	-e LDAP_USER_DC="people"                       \
