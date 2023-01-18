7f9cdefc36a9cac9fdfa0b68402fc16c6a9a06ac
Frizlab <fload@me.com>
2023-01-18 19:12:05 +0100

    Print users using TabularData when it’s available

--- Sources/officectl/Commands/officectl/users/list.swift
+++ Sources/officectl/Commands/officectl/users/list.swift
@@ -6,6 +6,9 @@
  */
 
 import Foundation
+#if canImport(TabularData)
+import TabularData
+#endif
 
 import ArgumentParser
 
@@ -31,8 +34,9 @@ struct List : AsyncParsableCommand {
 		try officectlOptions.bootstrap()
 		let officeKitServices = officectlOptions.officeKitServices
 		
-		let multiUsers = try await MultiServicesUser.fetchAll(in: officeKitServices.hashableUserServices(matching: usersOptions.serviceIDs), using: Officectl.services)
-		multiUsers.users.forEach{ multiUser in
+		let multiUsersResult = try await MultiServicesUser.fetchAll(in: officeKitServices.hashableUserServices(matching: usersOptions.serviceIDs), using: Officectl.services)
+#if !canImport(TabularData)
+		multiUsersResult.users.forEach{ multiUser in
 			print("---")
 			let maxLength = multiUser.keys.map(\.value.id.count).max() ?? 0
 			for key in (multiUser.keys.sorted{ $0.value.id < $1.value.id }) {
@@ -45,6 +49,25 @@ struct List : AsyncParsableCommand {
 				}
 			}
 		}
+#else
+		let multiUsers = multiUsersResult.users
+		let allServices = Set(multiUsers.flatMap{ $0.keys }).sorted{ $0.value.id < $1.value.id }
+		var dataFrame = DataFrame()
+		allServices.forEach{ service in
+			let rows = multiUsers.map{ $0[service] }.map{ userResult in
+				guard let userResult else {
+					return "<internal error>"
+				}
+				switch userResult {
+					case .success(nil):       return "<none>"
+					case .success(let user?): return "\(UserAndServiceFrom(user: user, service: service.value)!.taggedID.id)"
+					case .failure:            return "ERROR"
+				}
+			}
+			dataFrame.append(column: Column<String>(name: service.value.id, contents: rows))
+		}
+		print(dataFrame.sorted(on: "ggl").description(options: .init(maximumLineWidth: .max, maximumCellWidth: .max, maximumRowCount: .max, includesColumnTypes: false)))
+#endif
 	}
 	
 }
