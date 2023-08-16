f85b5b993cdc92918e06d0faac7eead11c621952
Frizlab <fload@me.com>
2023-08-17 01:51:18 +0200

    Add (unimplemented) experimental command to change the console permissions for happn

---
+++ Sources/officectl/Commands/officectl/experimental.swift
@@ -0,0 +1,27 @@
+/*
+ * experimental.swift
+ * officectl
+ *
+ * Created by François Lamboley on 2023/08/17.
+ */
+
+import Foundation
+
+import ArgumentParser
+import JWT
+
+
+
+struct Experimental : AsyncParsableCommand {
+	
+	static var configuration = CommandConfiguration(
+		abstract: "Experimental commands; use with care.",
+		shouldDisplay: false,
+		subcommands: [
+		]
+	)
+	
+	@OptionGroup()
+	var officectlOptions: Officectl.Options
+	
+}
---
+++ Sources/officectl/Commands/officectl/experimental/console-perm.swift
@@ -0,0 +1,46 @@
+/*
+ * console-perm.swift
+ * officectl
+ *
+ * Created by François Lamboley on 2023/08/17.
+ */
+
+import Foundation
+
+import ArgumentParser
+import Email
+import FormURLEncodedCoder
+import UnwrapOrThrow
+import URLRequestOperation
+
+import HappnOffice
+import OfficeKit
+
+
+
+struct ConsolePerm : AsyncParsableCommand {
+	
+	static var configuration = CommandConfiguration(
+		abstract: "Set the console permissions for a given user in the happn console."
+	)
+	
+	@OptionGroup()
+	var officectlOptions: Officectl.Options
+	
+	@Argument
+	var email: String
+	
+	func run() async throws {
+		try officectlOptions.bootstrap()
+		let officeKitServices = officectlOptions.officeKitServices
+		
+		guard let happnService = officeKitServices.hashableUserServices(matching: "hppn").onlyElement?.value as? HappnService else {
+			officectlOptions.logger.critical("happn service not found; bailing.")
+			throw ExitCode(1)
+		}
+		
+		officectlOptions.logger.critical("Not implemented")
+		throw ExitCode(1)
+	}
+	
+}
